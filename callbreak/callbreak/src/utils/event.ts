2a3551fdd9e775837e2d16b26172da9efa8d2d08
rudy <fluffyrudy@proton.me>
2025-09-25 22:23:00 +0545

    feat: add `Rect` class, `CustomEvent `class

---
+++ callbreak/callbreak/src/utils/event.ts
@@ -0,0 +1,41 @@
+import type { InputState } from "../types/types.type";
+
+export class CustomEvent {
+  private state: InputState = {
+    mouseX: -100,
+    mouseY: -100,
+    leftPressed: false,
+    rightPressed: false,
+    keysDown: new Set(),
+  };
+
+  constructor(canvas: HTMLCanvasElement) {
+    canvas.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+    });
+    canvas.addEventListener("mousemove", (e) => {
+      this.state.mouseX = e.offsetX;
+      this.state.mouseY = e.offsetY;
+    });
+
+    canvas.addEventListener("mousedown", (e) => {
+      if (e.button === 0) this.state.leftPressed = true;
+      if (e.button === 2) this.state.rightPressed = true;
+    });
+
+    canvas.addEventListener("mouseup", (e) => {
+      if (e.button === 0) this.state.leftPressed = false;
+      if (e.button === 2) this.state.rightPressed = false;
+    });
+    window.addEventListener("keydown", (e) => {
+      if (!this.state.keysDown.has(e.key)) {
+        this.state.keysDown.add(e.key);
+      }
+    });
+    window.addEventListener("keyup", (e) => this.state.keysDown.delete(e.key));
+  }
+
+  getState() {
+    return { ...this.state };
+  }
+}
---
+++ callbreak/callbreak/src/utils/rect.ts
@@ -0,0 +1,92 @@
+import type { Coor } from "../types/types.type";
+
+export class Rect {
+  public x: number;
+  public y: number;
+  public width = 0;
+  public height = 0;
+
+  constructor(x: number, y: number, w = 0, h = 0) {
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h;
+  }
+
+  get center(): Coor {
+    return {
+      x: this.x + Math.round(this.width / 2),
+      y: this.y + Math.round(this.height / 2),
+    };
+  }
+
+  get left() {
+    return this.x;
+  }
+  get right() {
+    return this.x + this.width;
+  }
+  get top() {
+    return this.y;
+  }
+  get bottom() {
+    return this.y + this.height;
+  }
+
+  set left(x: number) {
+    this.x = x;
+  }
+
+  set right(x: number) {
+    this.x = x - this.width;
+  }
+
+  set top(y: number) {
+    this.y = y;
+  }
+
+  set bottom(y: number) {
+    this.y = y - this.height;
+  }
+
+  set center(coor: Coor) {
+    this.x = Math.round(coor.x - this.width / 2);
+    this.y = Math.round(coor.y - this.height / 2);
+  }
+
+  scale(ratio: number) {
+    return {
+      w: this.width * ratio,
+      h: this.height * ratio,
+    };
+  }
+
+  scaleip(ratio: number) {
+    this.width = this.width * ratio;
+    this.height = this.height * ratio;
+    return this;
+  }
+
+  scaleAroundCenter(ratio: number) {
+    const c = this.center;
+    this.width *= ratio;
+    this.height *= ratio;
+    this.center = c;
+    return this;
+  }
+
+  collidepoint(x: Coor["x"], y: Coor["y"]) {
+    return (
+      x >= this.left && x <= this.right && y >= this.top && y <= this.bottom
+    );
+  }
+
+  colliderect(rect: Rect) {
+    return (
+      this.left < rect.right &&
+      rect.left < this.right &&
+      this.top < rect.bottom &&
+      rect.top < this.bottom
+    );
+  }
+}
